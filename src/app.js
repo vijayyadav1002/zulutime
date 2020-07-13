39aec8b2469ea2865c4ec1c5d58a842a9233927c
vijay <vijayyadav1002@gmail.com>
2020-07-13 13:16:59 -0400

    Checking mobile view

--- src/app.js
+++ src/app.js
@@ -4,12 +4,13 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import yellow from '@material-ui/core/colors/yellow';
+import './app.scss';
 
 const App = () => (
     <React.Fragment>
         <CssBaseline />
         <Container maxWidth="sm" disableGutters>
-            <Typography component="div" style={{ backgroundColor: yellow.A400, height: '100vh' }}>
+            <Typography component="div" style={{ backgroundColor: yellow.A400, height: '85vh' }}>
                 <ClockTimePicker />
             </Typography>
         </Container>
