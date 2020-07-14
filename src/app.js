05d5cd32a343edfb897f5ba270b969c25cc43777
vijay <vijayyadav1002@gmail.com>
2020-07-13 22:52:57 -0400

    Added service worker and eslint ignore

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ClockTimePicker from './components/clock-time-picker';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
@@ -6,15 +6,47 @@ import Container from '@material-ui/core/Container';
 import yellow from '@material-ui/core/colors/yellow';
 import './app.scss';
 
-const App = () => (
-    <React.Fragment>
-        <CssBaseline />
-        <Container maxWidth="sm" disableGutters>
-            <Typography component="div" style={{ backgroundColor: yellow.A400, height: '85vh' }}>
-                <ClockTimePicker />
-            </Typography>
-        </Container>
-    </React.Fragment>
-);
+// Hook
+function useWindowSize() {
+    const isClient = typeof window === 'object';
+
+    function getSize() {
+        return {
+            width: isClient ? window.innerWidth : undefined,
+            height: isClient ? window.innerHeight : undefined
+        };
+    }
+
+    const [windowSize, setWindowSize] = useState(getSize);
+
+    useEffect(() => {
+        if (!isClient) {
+            return false;
+        }
+
+        function handleResize() {
+            setWindowSize(getSize());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []); // Empty array ensures that effect is only run on mount and unmount
+
+    return windowSize;
+}
+
+const App = () => {
+    const size = useWindowSize();
+    return (
+        <React.Fragment>
+            <CssBaseline/>
+            <Container maxWidth="sm" disableGutters>
+                <Typography component="div" style={{ backgroundColor: yellow.A400, height: size.height }}>
+                    <ClockTimePicker/>
+                </Typography>
+            </Container>
+        </React.Fragment>
+    );
+};
 
 export default App;
---
+++ src/public/service-worker.js
@@ -0,0 +1,67 @@
+var CACHE_NAME = 'zulutime.v1';
+var urlsToCache = [
+    '/'
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+
+                return fetch(event.request).then(
+                    function(response) {
+                        // Check if we received a valid response
+                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // IMPORTANT: Clone the response. A response is a stream
+                        // and because we want the browser to consume the response
+                        // as well as the cache consuming the response, we need
+                        // to clone it so we have two streams.
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function(cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+    var cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
